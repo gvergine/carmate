b503c82ba1c2909120392cb3ae914a6c1f6af074
Giovanni Vergine <verginegiovanni@gmail.com>
2019-08-26 23:22:45 +0300

    Synchronize according to generation

--- src/carmate/src/carmate.h
+++ src/carmate/src/carmate.h
@@ -9,17 +9,17 @@ extern void loop_tracker();
 extern void loop_clock();
 extern void loop_syncer();
 
-#define QUEUE_SYNCER_BUFFER_SIZE 1*1024
-#define QUEUE_TRACKER_BUFFER_SIZE 1*1024
+#define QUEUE_TRACKER_BUFFER_SIZE 16384
+#define QUEUE_SYNCER_BUFFER_SIZE 1024
 
-static uint8_t queue_syncer_buffer[QUEUE_SYNCER_BUFFER_SIZE];
 static uint8_t queue_tracker_buffer[QUEUE_TRACKER_BUFFER_SIZE];
+static uint8_t queue_syncer_buffer[QUEUE_SYNCER_BUFFER_SIZE];
 
-#define QUEUE_SYNCER_ELEMENT_SIZE 1
-#define QUEUE_TRACKER_ELEMENT_SIZE sizeof(struct gps_info)
+#define QUEUE_TRACKER_ELEMENT_SIZE sizeof(union __u1{struct gps_info __t10;})
+#define QUEUE_SYNCER_ELEMENT_SIZE sizeof(union __u3{uint8_t __t30;uint8_t __t31;})
 
+struct queue queue_tracker = QUEUE_INIT(QUEUE_TRACKER_BUFFER_SIZE,QUEUE_TRACKER_ELEMENT_SIZE,queue_tracker_buffer);
 struct queue queue_syncer = QUEUE_INIT(QUEUE_SYNCER_BUFFER_SIZE,QUEUE_SYNCER_ELEMENT_SIZE,queue_syncer_buffer);
-struct queue queue_tracker = QUEUE_INIT(QUEUE_TRACKER_BUFFER_SIZE,QUEUE_TRACKER_ELEMENT_SIZE,queue_syncer_buffer);
 
 struct queue * queue_gps_reader_gps_update = &queue_tracker;
 struct queue * queue_tracker_sync_signal = &queue_syncer;
